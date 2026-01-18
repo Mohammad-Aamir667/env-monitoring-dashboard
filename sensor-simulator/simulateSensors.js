11923ecf78d187d0105a114aa3ddcef977c33f68
Mohammad Aamir <aamireverlasting786>
2026-01-18 19:15:58 +0530

    added script for sending sensors data via socket

---
+++ sensor-simulator/simulateSensors.js
@@ -0,0 +1,15 @@
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+setInterval(() => {
+    const sensors = [
+        { sensorType: "temperature", value: 20 + Math.random() * 25 },
+        { sensorType: "humidity", value: 40 + Math.random() * 40 },
+        { sensorType: "airQuality", value: 80 + Math.random() * 200 }
+    ];
+
+    sensors.forEach(s =>
+        socket.emit("sensor:data", s)
+    );
+}, 4000);
