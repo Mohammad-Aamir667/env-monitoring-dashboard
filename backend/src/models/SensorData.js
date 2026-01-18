16f88ee03afc1665fc5967afda62ef11053ce000
Mohammad Aamir <aamireverlasting786>
2026-01-19 02:36:25 +0530

    added collection name

--- backend/src/models/SensorData.js
+++ backend/src/models/SensorData.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const sensorSchema = new mongoose.Schema({
     sensorType: String,
     value: Number,
-    timestamp: { type: Date, default: Date.now }
-});
+
+    timestamp: { type: Date, default: Date.now },
+
+}, { collection: "sensor_data" });
 
 export default mongoose.model("SensorData", sensorSchema);
